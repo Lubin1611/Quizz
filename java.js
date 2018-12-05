fb71a76c92288e69ca911e66e667d12037a2c7e6
Lubin1611 <43751457+Lubin1611@users.noreply.github.com>
2018-12-05 16:41:17 +0100

    Quizz v2

--- java.js
+++ java.js
@@ -7,6 +7,7 @@ var arrQuestions = [
         "reponse3": "Dracaufeu",
         "reponse4": "Chépo",
         "bonnereponse": "reponse2"
+
     },
     {
         "intitule": "Question 2",
@@ -36,42 +37,61 @@ var arrQuestions = [
         "bonnereponse": "reponse3"
     },
     {
-        "intitule": "Question 2",
-        "question": "Comme s'apelle ta formation ?",
-        "reponse1": "la croisière s'amuse",
-        "reponse2": "Uptofourmies",
-        "reponse3": "Simon academy",
-        "reponse4": "Les bidasses en vadrouille",
+        "intitule": "Question 5",
+        "question": "Sur quelle planète nous vivons ?",
+        "reponse1": "La terre",
+        "reponse2": "Venus",
+        "reponse3": "Bisounours",
+        "reponse4": "Planète a sec",
+        "bonnereponse": "reponse1"
+    },
+    {
+        "intitule": "Question 6",
+        "question": "En quelle année seront nous l'annee prochaine ?",
+        "reponse1": "2090",
+        "reponse2": "2019",
+        "reponse3": "1977",
+        "reponse4": "50 av. JC",
         "bonnereponse": "reponse2"
     },
     {
-        "intitule": "Question 2",
-        "question": "Comme s'apelle ta formation ?",
-        "reponse1": "la croisière s'amuse",
-        "reponse2": "Uptofourmies",
-        "reponse3": "Simon academy",
-        "reponse4": "Les bidasses en vadrouille",
+        "intitule": "Question 7",
+        "question": "Qu'est ce qui a été cassé ce matin ?",
+        "reponse1": "Une table",
+        "reponse2": "Une vitre",
+        "reponse3": "Un ordi",
+        "reponse4": "Une tasse",
         "bonnereponse": "reponse2"
     },
     {
-        "intitule": "Question 2",
-        "question": "Comme s'apelle ta formation ?",
-        "reponse1": "la croisière s'amuse",
-        "reponse2": "Uptofourmies",
-        "reponse3": "Simon academy",
-        "reponse4": "Les bidasses en vadrouille",
-        "bonnereponse": "reponse2"
+        "intitule": "Question 8",
+        "question": "Quelle est la masse du trou noir de notre galaxie ? (En soleils)",
+        "reponse1": "10",
+        "reponse2": "10.000",
+        "reponse3": "100.000",
+        "reponse4": "4,2 millions",
+        "bonnereponse": "reponse4"
     },
     {
-        "intitule": "Question 2",
-        "question": "Comme s'apelle ta formation ?",
-        "reponse1": "la croisière s'amuse",
-        "reponse2": "Uptofourmies",
-        "reponse3": "Simon academy",
-        "reponse4": "Les bidasses en vadrouille",
-        "bonnereponse": "reponse2"
+        "intitule": "Question 9",
+        "question": "Combien avons-nous de cheveux ? (En moyenne)",
+        "reponse1": "10 milliards",
+        "reponse2": "entre 10.000 et 15 000",
+        "reponse3": "entre 100.000 et 150 000",
+        "reponse4": "4,2 millions",
+        "bonnereponse": "reponse4"
     },
+    {
+        "intitule": "Question 10",
+        "question": "0 + 0 = ?",
+        "reponse1": "10",
+        "reponse2": "0",
+        "reponse3": "captcha reussi",
+        "reponse4": "4,2 millions",
+        "bonnereponse": "reponse3"
+    }
 ];
+
 
 
 $("#questions").html(arrQuestions[0].intitule + " " + arrQuestions[0].question);
@@ -83,25 +103,56 @@ $("#reponse4").html(arrQuestions[0].reponse4);
 
 
 
+
 var index = 0;
 
 $("#reponse1, #reponse2, #reponse3, #reponse4").on("click", function () {
 
 
-    if (index == 0) {
+    //for (var i = 0; i < arrQuestions.length; i++) {
 
-        i = 1;
 
-        $("#questions").html(arrQuestions[i].intitule + " " + arrQuestions[i].question);
+       // if (index == i) {
 
-        $("#reponse1").html(arrQuestions[i].reponse1);
-        $("#reponse2").html(arrQuestions[i].reponse2);
-        $("#reponse3").html(arrQuestions[i].reponse3);
-        $("#reponse4").html(arrQuestions[i].reponse4);
+            //i++;
 
-    }
+            //verification de la réponse
+            console.log(this.id);
+
+            if (this.id == arrQuestions[index].bonnereponse) {
+
+                alert("bonne reponse !");
+
+            }
+
+            // Passage a la question suivante
+            index++;
+
+            $("#questions").html(arrQuestions[index].intitule + " " + arrQuestions[index].question);
+
+            $("#reponse1").html(arrQuestions[index].reponse1);
+            $("#reponse2").html(arrQuestions[index].reponse2);
+            $("#reponse3").html(arrQuestions[index].reponse3);
+            $("#reponse4").html(arrQuestions[index].reponse4);
+
+
+        //}
+
+    //}
+
 
 });
+
+console.log($("#reponse2").text());
+
+
+
+
+
+
+
+
+
 
 /*var $coucou = document.getElementById("coucou");
 
